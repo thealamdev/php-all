56c10037c13c6509f65206d9bbf105e612da7f1a
Shah Alam <shahalam141072@gmail.com>
2023-02-12 15:16:44 +0600

    constant done

--- constant/constant.php
+++ constant/constant.php
@@ -1,6 +1,23 @@
 <?php
 define('TEST',50);
-$first = 60;
+$first = 50;
 echo TEST+$first;
 
+if(TEST <> $first){
+    echo "this is not equal";
+}else{
+   echo "this is equal";
+}
+
+function myFun(){
+    global $first;
+    echo "this is number " . $first . "<br>";
+}
+
+myFun();
+
+
+echo "this" , "is","shahalam" . "<br>";
+print("this " . "is");
+print "<h2>".$first. "</h2>"
 ?>
