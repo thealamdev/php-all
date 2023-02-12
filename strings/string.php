ef71d796175ec237c3b86ff1c76d0122e2f8e317
Shah Alam <shahalam141072@gmail.com>
2023-02-12 15:44:08 +0600

    string done

---
+++ strings/string.php
@@ -0,0 +1,12 @@
+<?php
+#taking a input: 
+ $string = "Hello Bangladesh and all the people";
+ echo strlen($string)."<br>";
+ echo str_word_count($string)."<br>";
+ echo strrev($string)."<br>";
+ echo strpos($string,"all the")."<br>";
+ echo str_replace("Bangladesh","world",$string);
+
+  
+
+?>
