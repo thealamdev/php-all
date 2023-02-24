f4ee5448ca22743314c2a39986208efc85ffbdb1
Shah Alam <shahalam141072@gmail.com>
2023-02-24 21:57:29 +0600

    function is done

---
+++ function/function.php
@@ -0,0 +1,15 @@
+<?php
+
+#function without parameter:
+// declaration of a function:
+
+function myFunction()
+{
+    echo "Hello everyone <br>";
+}
+
+
+
+// call a function:
+myFunction();
+myFunction();
