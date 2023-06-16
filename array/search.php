ec9710e9aaddfe32954d7d5de4e0b1e119997324
Shah Alam <shahalam141072@gmail.com>
2023-06-17 02:39:14 +0600

    search array done

---
+++ array/search.php
@@ -0,0 +1,25 @@
+<?php
+
+$array = [
+    'name' => 'Shah alam',
+    'mark' => 39,
+    'age' => 23,
+    'dep' => 'cse',
+    'address' => 'Dhaka',
+];
+
+
+echo "<pre>";
+    print_r($array);
+echo "</pre>";
+
+// search array index or key by array_search() function:
+
+echo array_search('Shah alam',$array,true);
+echo "<br>";
+// search an value in array and if find then give ture(1) and if not find then give false(0);
+
+echo in_array("39",$array,true);
+
+// we can strict mood in in_array(). it means if we use strict mood then we need to pass one extra parameter and the data type also be need to match.
+
