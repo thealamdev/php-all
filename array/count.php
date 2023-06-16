6dc8657d4af88041c7713643873a78d764eafd8d
Shah Alam <shahalam141072@gmail.com>
2023-06-17 02:31:41 +0600

    done

---
+++ array/count.php
@@ -0,0 +1,25 @@
+<?php
+
+$array = [
+    [1,2,3,4,5,6,6],
+    [1,2,3,4,5,6,6],
+    [1,2,3,4,5,6,6],
+    [1,2,3,4,5,6,6],
+    [1,2,3,4,5,6,6],
+    [1,2,3,4,5,6,6],
+    [1,2,3,4,5,6,6],
+    [1,2,3,4,5,6,6]
+];
+
+$result = [
+    1,2,3,2,6,1,1
+];
+
+echo count($array);
+echo sizeof($array);
+
+echo "<pre>";
+print_r(array_count_values($result));
+echo "</pre>";
+
+// array_count_values help us to find the repeted value of a array:
