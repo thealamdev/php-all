6b819cb3bfd20b7d818bd2f0fbedea21401a125d
Shah Alam <shahalam141072@gmail.com>
2023-06-16 13:39:27 +0600

    array done

---
+++ array/multi.php
@@ -0,0 +1,17 @@
+<?php
+
+$array = [
+    'name' => 'Shah Alam',
+    'age' => 21,
+    'dep' => 'CSE',
+    'education' => [
+        'ssc' => 'tilli high school',
+        'hsc' => 'Khan Bahadur Awlad Hossain khan college',
+        'versity' => 'City University',
+    ],
+];
+
+
+echo "<pre>";
+    print_r($array);
+echo "</pre>";
