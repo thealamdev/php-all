6bac0e4e4261beed0a6d6ba4f676386558624e4e
Shah Alam <shahalam141072@gmail.com>
2023-06-17 01:32:18 +0600

    done

--- array/list.php
+++ array/list.php
@@ -8,11 +8,75 @@ $emp = [
 ];
 
 
-foreach($emp as list($id,$name,$deg,$salary)){
-    echo "$id $name $deg $salary <br>";
+$emp = [
+    [
+        'id' => 1,
+        'name' => 'Shah Alam',
+        'deg' => "WEB DEVELOPER",
+        'salary' => 20000
+    ],
+    [
+        'id' => 2,
+        'name' => 'John Doe',
+        'deg' => "SOFTWARE ENGINEER",
+        'salary' => 25000
+    ],
+    [
+        'id' => 3,
+        'name' => 'Jane Smith',
+        'deg' => "GRAPHIC DESIGNER",
+        'salary' => 18000
+    ],
+    [
+        'id' => 4,
+        'name' => 'Mike Johnson',
+        'deg' => "DATA ANALYST",
+        'salary' => 22000
+    ]
+];
+
+
+$emp = [
+    [
+        'id' => 1,
+        'name' => 'Shah Alam',
+        'deg' => "WEB DEVELOPER",
+        'salary' => 20000
+    ],
+    [
+        'id' => 2,
+        'name' => 'John Doe',
+        'deg' => "SOFTWARE ENGINEER",
+        'salary' => 25000
+    ],
+    [
+        'id' => 3,
+        'name' => 'Jane Smith',
+        'deg' => "GRAPHIC DESIGNER",
+        'salary' => 18000
+    ],
+    [
+        'id' => 4,
+        'name' => 'Mike Johnson',
+        'deg' => "DATA ANALYST",
+        'salary' => 22000
+    ]
+];
+
+$empObjects = [];
+foreach ($emp as $employee) {
+    $empObject = new stdClass();
+    $empObject->id = $employee['id'];
+    $empObject->name = $employee['name'];
+    $empObject->deg = $employee['deg'];
+    $empObject->salary = $employee['salary'];
+    $empObjects[] = $empObject;
+    print_r($empObject);
 }
 
+
  
+
 
 ?>
 
@@ -26,7 +90,7 @@ foreach($emp as list($id,$name,$deg,$salary)){
     </thead>
 
     <tbody>
-        <?php foreach($emp as list($id,$name,$deg,$salary)){?>
+        <?php foreach($emp as list('id'=> $id,'name'=>$name, 'deg' => $deg, 'salary'=> $salary)){?>
             <tr>
             <td><?php echo $id ?></td>
             <td><?php echo $name ?></td>
