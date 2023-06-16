06044e83bd1acfd6e4f206c8d000ca627e299465
Shah Alam <shahalam141072@gmail.com>
2023-06-17 00:48:22 +0600

    done

--- array/multi.php
+++ array/multi.php
@@ -12,6 +12,31 @@ $array = [
 ];
 
 
+$results = [
+    'akash' => [
+        'bangla' => 76,
+        'english' => 98, 
+        'islam' => 70, 
+        'physics' => 80, 
+    ],
+
+    'kumar' => [
+        'bangla' => 75,
+        'english' => 43, 
+        'islam' => 65, 
+        'physics' => 43, 
+    ],
+
+];
+
+foreach($results as $result){
+     
+    foreach($result as $r){
+        echo $r . "<br>";
+    }
+}
+
+
 echo "<pre>";
     print_r($array);
 echo "</pre>";
