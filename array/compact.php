a61abde422da4731c85625951b5cf7aa83d14dfc
Shah Alam <shahalam141072@gmail.com>
2023-06-17 11:43:22 +0600

    array keys and array values done

--- array/compact.php
+++ array/compact.php
@@ -10,3 +10,16 @@ $infos = compact('name','age','dep');
 echo "<pre>";
 print_r($infos);
 echo "</pre>";
+
+
+$keys = array_keys($infos);
+$values = array_values($infos);
+
+print_r($keys);
+echo "<br>";
+print_r($values);
+echo "<br>";
+
+for($i=0;$i<count($infos);$i++){
+    echo $keys[$i] . " = " . $values[$i] . "<br>";
+}
