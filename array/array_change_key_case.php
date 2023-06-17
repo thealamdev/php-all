f0eb925e0ab05914ed2e0d7fd292d7d0e6a5885b
Shah Alam <shahalam141072@gmail.com>
2023-06-18 02:41:39 +0600

    done:

--- array/array_change_key_case.php
+++ array/array_change_key_case.php
@@ -6,6 +6,13 @@ $array = [
     'dep' => 'cse',
 ];
 
+$array2 =[
+    'address' => 'Dhaka',
+    'zip' => 1800
+];
+
+$array3 = array_merge($array,$array2); // must be two array parameter will be array:
 echo "<pre>";
    print_r(array_change_key_case($array,CASE_LOWER));
+   print_r($array3);
 echo"</pre>";
