8f832def585469c5eea7c9f206a25f1a4749e6d5
Shah Alam <shahalam141072@gmail.com>
2023-06-18 04:05:17 +0600

    done:

---
+++ array/array_merage.php
@@ -0,0 +1,24 @@
+<?php
+
+/* array merge: we can merge array by three way 
+i. array_merge() : it can merge index array and associative array. if there is any common key it will replace and then merge.
+ii. array_merge_recursive() : it basically use in multidimentional associtaive array.
+iii. array_combine() : it can merge indexing array.
+
+we will learn about them and solve problem.
+*/
+
+
+$fruits = [
+    'a' => 'apple',
+    'b' => 'bannana',
+    'c' => 'carrots',
+];
+
+$color = [
+    'r' => 'red',
+    'b' => 'blue',
+    'g' => 'green'
+];
+
+
