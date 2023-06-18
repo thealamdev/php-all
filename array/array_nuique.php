944162e99c80cb9f6655726aca8bcb4a354c5843
Shah Alam <shahalam141072@gmail.com>
2023-06-18 14:23:44 +0600

    done

---
+++ array/array_nuique.php
@@ -0,0 +1,13 @@
+<?php
+
+$arr = [
+    'name' => 'Shah alam',
+    'name' => 'Shah',
+    'age' => 'Shah alam',
+    'name' => 'Shah alam',
+    'name' => 'Shah alam',
+];
+
+$new = array_unique($arr);
+
+print_r($arr);
