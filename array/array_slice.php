83a0d46cf856658d16ff6814a70597c8064ed297
Shah Alam <shahalam141072@gmail.com>
2023-06-18 13:49:59 +0600

    done

--- array/array_slice.php
+++ array/array_slice.php
@@ -6,6 +6,19 @@ array_slice(array, start, length, preserve key); preserve key take two value(tru
 if we given true it will take main array key other wise it will take new key
 */
 
+
+
+/*  
+array_splice() it work for deleting some value form anywhere.
+Note: array_push() : it work for enter any value in the last.
+array_pop() : it work for delete any value in the last.
+array_shift() : it work for delete value form first.
+array_unshift() : it work for insert value into first.
+array_slice() : it will cut a portion of an array.
+array_splice() : it will delete a value or more value form any where.
+*/
+
+
 $array = [
     'mehedi',
     'papiya',
@@ -13,10 +26,12 @@ $array = [
     'big' => 'shah alam',
      45 =>'papiya',
 ];
-
-
+// array_pop($array);
+// print_r($array);
 $newArray = array_slice($array,2,3);
+array_splice($array,1);
 
 echo "<pre>";
 print_r($newArray);
+print_r($array);
 echo "</pre>";
