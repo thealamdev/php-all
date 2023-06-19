b0c1120e9ebbb92adcd236ee4be6f41cb852a9a7
Shah Alam <shahalam141072@gmail.com>
2023-06-19 22:30:03 +0600

    done

--- array/array_column.php
+++ array/array_column.php
@@ -1,25 +1,27 @@
 <?php
+// An array that represents a possible record set returned from a database
+$a = array(
+  array(
+    'id' => 5698,
+    'first_name' => 'Peter',
+    'last_name' => 'Griffin',
+    'option' => [
+        'first_name' => "Shah alam",
+        'last_name' => "Khan",
+        ]
+  ),
+  array(
+    'id' => 4767,
+    'first_name' => 'Ben',
+    'last_name' => 'Smith',
+  ),
+  array(
+    'id' => 3809,
+    'first_name' => 'Joe',
+    'last_name' => 'Doe',
+  )
+);
 
-$arr = [
-    [
-        'name' => 'shah',
-        'id' => 21,
-        'dep' => 'cse',
-    ],
-    [
-        'name' => 'shah alam',
-        'id' => 25,
-        'dep' => 'cse',
-    ],
-    [
-        'name' => 'alam',
-        'id' => 70,
-        'dep' => 'cse',
-    ]
-
-];
-
-echo "<pre>";  
- print_r(array_column($arr,'name','id'));
-
- echo"</pre>";
+$last_names = array_column($a, 'last_name');
+print_r($last_names);
+?>
