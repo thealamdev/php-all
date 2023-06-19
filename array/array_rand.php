1e4197ce3ae39806abae57118f46c91173d63e3c
Shah Alam <shahalam141072@gmail.com>
2023-06-19 23:33:25 +0600

    done

--- array/array_rand.php
+++ array/array_rand.php
@@ -1,8 +1,39 @@
 <?php
-$a = [ 
-    10,20,40,59,10,20,40,59
+$a = [
+    1,2,3,4,5,6
 ];
 
 $new = array_rand($a);
 
-echo $new;
+
+
+if ($new == 1) { ?>
+    <img src="https://t3.ftcdn.net/jpg/01/97/16/30/360_F_197163001_H2gm3LqIrVTm7Fne6kclwRDfMcfm57BC.jpg" alt="">
+<?php
+}
+
+if ($new == 2) { ?>
+    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+<?php
+}
+
+if ($new == 3) { ?>
+    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+<?php
+}
+
+if ($new == 4) { ?>
+    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+<?php
+}
+
+if ($new == 5) { ?>
+    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+<?php
+}
+
+if ($new == 6) { ?>
+    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+<?php
+}?>
+
