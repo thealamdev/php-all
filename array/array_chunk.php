1c940d84c7a3528711abfd5028beee4b8d2b015c
Shah Alam <shahalam141072@gmail.com>
2023-06-19 22:47:07 +0600

    done

---
+++ array/array_chunk.php
@@ -0,0 +1,15 @@
+<?php
+
+$arr = [
+    'name' => 'SHAH ALAM',
+    'age' => 21,
+    'department' => 'cse',
+    'email' => 'shahal@gmail.com',
+    'phone' => 378,
+];
+
+$new = array_chunk($arr,3,true);
+
+echo "<pre>";
+print_r($new);
+echo"</pre>";
