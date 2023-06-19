fca7cea6f85dab320bb72e9387036a1330e48df0
Shah Alam <shahalam141072@gmail.com>
2023-06-19 23:37:20 +0600

    done

--- array/array_rand.php
+++ array/array_rand.php
@@ -8,32 +8,32 @@ $new = array_rand($a);
 
 
 if ($new == 1) { ?>
-    <img src="https://t3.ftcdn.net/jpg/01/97/16/30/360_F_197163001_H2gm3LqIrVTm7Fne6kclwRDfMcfm57BC.jpg" alt="">
+    <img src="https://t3.ftcdn.net/jpg/01/97/16/30/360_F_197163001_H2gm3LqIrVTm7Fne6kclwRDfMcfm57BC.jpg" alt="" width="100px">
 <?php
 }
 
 if ($new == 2) { ?>
-    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="" width="100px">
 <?php
 }
 
 if ($new == 3) { ?>
-    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+    <img src="https://www.media4math.com/sites/default/files/library_asset/images/MathClipArt--Single-Die-with-3-Showing.png" alt="" width="100px">
 <?php
 }
 
 if ($new == 4) { ?>
-    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+    <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQTxvpQM9Iac7bbFvYL1KoVsEqIn4odj2IMMFPvNqrGY-xci_Zh1xPCan7c9s9mfEVc3Ow&usqp=CAU" alt="" width="100px">
 <?php
 }
 
 if ($new == 5) { ?>
-    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+    <img src="https://www.clker.com/cliparts/e/y/7/h/W/K/dice-5-hi.png" alt="" width="100px">
 <?php
 }
 
 if ($new == 6) { ?>
-    <img src="https://static.thenounproject.com/png/1194703-200.png" alt="">
+    <img src="https://image.winudf.com/v2/image1/Y29tLm9yYW5nZWxvLmRpY2Vfc2NyZWVuXzBfMTU5NzQ4Mzk5MF8wOTg/screen-0.webp?fakeurl=1&type=.webp" alt="" width="100px">
 <?php
 }?>
 
