d1f80395863cf9ce115e324149ca9e4532dc1dda
Shah Alam <shahalam141072@gmail.com>
2023-06-21 02:24:37 +0600

    done

---
+++ Date/date.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/* in this tutorial we will learn about php date function and it's working principal: */
+
+
+// THIS IS FOR DAYS:
+echo "d = ". date('d') . "<br>";
+echo 'j =' . date('j') . "<br>";
+echo 'S =' . date('S') . "<br>";
+
+// THIS IS FOR MONTH:
+echo 'm =' . date('m') . "<br>";
+echo 'M =' . date('M') . "<br>";
+echo 'n =' . date('n') . "<br>";
+echo 'F =' . date('F') . "<br>";
+
+// THIS IS FOR YEAR:
+echo 'y =' . date('y') . "<br>";
+echo 'Y =' . date('Y') . "<br>";
+
+// THIS IS FOR WEEK:
+echo 'w =' . date('w') . "<br>"; 
+echo 'N =' . date('N') . "<br>"; 
+echo 'D =' . date('D') . "<br>"; 
+echo 'l =' . date('l') . "<br>"; 
+
+
+
