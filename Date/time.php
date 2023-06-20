f150bc10773ee492d57d621b79db82e3c7936b72
Shah Alam <shahalam141072@gmail.com>
2023-06-21 04:43:49 +0600

    done

--- Date/time.php
+++ Date/time.php
@@ -2,8 +2,13 @@
 
 date_default_timezone_set('Asia/Dhaka');
 echo date_default_timezone_get() . "<br>";
-echo "second is " . date('h e') . "<br>";
-echo "second is " . date('i') . "<br>";
+echo "hour is " . date('h') . "<br>";
+echo "hour is " . date('H') . "<br>";
+echo "hour is " . date('G') . "<br>";
+echo "hour is " . date('g') . "<br>";
+
+echo "minutes is " . date('i') . "<br>";
 echo "second is " . date('s') . "<br>";
+echo "Merideam is " . date('A') . "<br>";
 
 echo date('z');
