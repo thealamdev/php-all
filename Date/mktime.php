34af3f45d80870d8dc05388b71cae41277e75dc3
Shah Alam <shahalam141072@gmail.com>
2023-06-21 15:33:50 +0600

    done

--- Date/mktime.php
+++ Date/mktime.php
@@ -2,6 +2,6 @@
 
 // by using mkdate()  or gmmktime() we can print anything of privious date. For Example, i want to show what is the week name of my birthday.
 date_default_timezone_set('Asia/Dhaka');
-$date = gmmktime('6','10','59','11','28','1999');
+$date = gmmktime('2','10','59','11','28','2333');
 
-echo "I born in " . date('l',$date);
+echo "I born in " . date('Y-m-d',$date);
