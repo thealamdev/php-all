5e7982f7a41d1311e57cd887e08f98288510f140
Shah Alam <shahalam141072@gmail.com>
2023-07-07 00:22:41 +0600

    done

--- Date/date_add.php
+++ Date/date_add.php
@@ -3,6 +3,7 @@
 $date = date_create('2023-05-15');
 
 $new = date_sub($date,date_interval_create_from_date_string("5  months"));
+$newDate = date_modify($date,"10 days");
 
 echo "<pre>";
 print_r( date_interval_create_from_date_string('5 months'));
@@ -10,5 +11,6 @@ print_r(date_add($date,date_interval_create_from_date_string("30 days")));
  
 echo "</pre>";
 
-echo date_format($new, 'd-m-y');
+// echo date_format($date)
+echo date_format($newDate, 'd-m-y');
 
