3a6a8e4a1a6ebf49bd49b8140ca7cfb5ce5a0e40
Shah Alam <shahalam141072@gmail.com>
2023-07-07 11:10:12 +0600

    done

--- Date/localtime.php
+++ Date/localtime.php
@@ -4,4 +4,11 @@ $date = localtime(time(),true);
 
 echo "<pre>";
     print_r($date);
+    echo date('Y-m-d');
+    echo "<br>";
+    echo date('Y-m-d',strtotime('5 march 2098'));
+    echo "<br>";
+    echo date('Y-m-d h:i',strtotime('+5 days'));
+    echo "<br>";
+    echo date('Y-m-d h:i',strtotime('last year'));
 echo"</pre>";
